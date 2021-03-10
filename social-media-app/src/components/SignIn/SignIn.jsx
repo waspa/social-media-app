da6294a9895c9a7e2633f5244187effda842fabc
Ernesto Neyra <ernestoneyra31@gmail.com>
2021-03-10 16:15:27 +0100

    add firebase

--- social-media-app/src/components/SignIn/SignIn.jsx
+++ social-media-app/src/components/SignIn/SignIn.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import "./SignIn.css";
 import { Link } from "react-router-dom";
 
 function SignIn() {
   return (
-    <div className="text-center centered">
+    <div className="text-center centered signin">
       <main className="form-signin">
         <form>
           <img
@@ -13,40 +13,37 @@ function SignIn() {
             alt="logo"
           />
           <h1 className="h3 mb-3">Sign in</h1>
-          <label for="inputEmail" className="visually-hidden"></label>
-          <input
-            type="username"
-            id="inputEmail"
-            className="form-control"
-            placeholder="Username"
-            required
-            autofocus
-          />
-          <label for="inputPassword" className="visually-hidden">
-            Password
-          </label>
 
           <input
+            type="username"
+            required
+            className="form-control"
+            placeholder="Username"
+          />
+          <input
             type="password"
-            id="inputPassword"
             className="form-control"
             placeholder="Password"
             required
           />
-
           <div className="checkbox mb-3">
             <label>
               <input type="checkbox" value="remember-me" className="me-2" />
               <span>Remember Me</span>
             </label>
           </div>
+
           <button className="w-100 btn btn-lg btn-primary" type="submit">
             Sign in
           </button>
         </form>
       </main>
       <nav>
-        <Link to="/registeraccount">No acount? Create account here</Link>
+        <Link to="/registeraccount">Create a new account here </Link>
+        {/* <Link to="/forgotpassword">Forgot your password?</Link> */}
+        <div>
+          <em>Forgot your password?</em>
+        </div>
       </nav>
     </div>
   );
