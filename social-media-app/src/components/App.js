4ad5792da7796c7135e3cb63cbb394f0e0e738b9
Ernesto Neyra <ernestoneyra31@gmail.com>
2021-03-12 16:13:38 +0100

    Added login, logout, sign up, reset password

---
+++ social-media-app/src/components/App.js
@@ -0,0 +1,41 @@
+import Signup from "./Signup";
+import React from "react";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "../components/Dashboard";
+import Login from "../components/Login";
+import PrivateRoute from './PrivateRoute'
+import ForgotPassword from './ForgotPassword'
+import UpdateProfile from "./UpdateProfile";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <Route  path="/update-profile" component={UpdateProfile} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+    </AuthProvider>
+  );
+}
+
+export default App;
